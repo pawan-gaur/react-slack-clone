47358aa6b3a2aaca35d94e27ba890ec262b6b73c
Pawan Gaur <pawangaur001@gmail.com>
2020-06-19 12:02:12 +0530

    3. User Authentication with Firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+var firebaseConfig = {
+  apiKey: { API_KEY },
+  authDomain: { AUTH_DOMAIN },
+  databaseURL: { DATABASE_URL },
+  projectId: { PROJECT_ID },
+  storageBucket: { STORAGE_BUCKET },
+  messagingSenderId: { MESSAGING_SENDER_ID },
+  appId: { APP_ID },
+  measurementId: { MEASUREMENT_ID },
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
+
+export default firebase;
